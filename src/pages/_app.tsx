0c149dd5fefb2384ebe78f34486a67a6a038a9d9
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-02-04 13:04:25 -0300

    Material UI installed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
+//import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
+import CssBaseline from '@mui/material/CssBaseline';
+import darkScrollbar from '@mui/material/darkScrollbar';
+const theme = createTheme({
+  palette: {
+   mode:'dark'
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme} >
+      <CssBaseline>
+  <Component {...pageProps} />
+  </CssBaseline>
+  </ThemeProvider>
+  )
 }
