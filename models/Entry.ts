433c518d8ed84741834b59f40a2e70fe6704bc2e
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-02-08 15:32:20 -0300

    End point to get all inputs

--- models/Entry.ts
+++ models/Entry.ts
@@ -2,7 +2,7 @@
 import mongoose, { Model, Schema } from "mongoose";
 import { Entry } from '../interfaces/entries';
 
-interface IEntry extends Entry {
+export interface IEntry extends Entry {
 }
 
 const entrySchema= new Schema( {
