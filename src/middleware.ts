e34de2eff6bf49db11a8c2bc811d9f226661960e
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-03-15 16:31:20 -0300

    middleware blocked

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
- 
+  console.log(req.nextUrl.pathname)
   // if( req.nextUrl.pathname.startsWith('/api/entries/') ){
   //   const id = req.nextUrl.pathname.replace('/api/entries/','');
   //   const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
